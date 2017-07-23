0144ebbf592f0a5da92d8d5e851e6f4b02dc961b
oopfuture <oopfuture@gmail.com>
2017-07-23 18:14:51 +0800

    图书列表

--- server/services/things.js
+++ server/services/things.js
@@ -22,8 +22,29 @@ var host = "http://211.149.248.241:16004/";
 
 var nav = function(server) {
     return {
-        get_car_by_person: function(person_id,cb) {
-            var url = host + "car/get_by_person?person_id=" + person_id;
+        list_books: function(org_code,params,cb) {
+            if (!params) {
+                params = "";
+            }
+            var url = host + "book/list_books?org_code=" + org_code + "&params=" + params;
+            uu_request.get(url, function(err, response, body) {
+                if (!err && response.statusCode === 200) {
+                    var info = JSON.parse(body);
+
+                    var rows = [];
+                    if (info.success) {
+                        rows = info.rows;
+                    }
+                    cb(err,rows);
+                } else {
+                    cb(true,{message:"网络错误"});
+                }
+            });
+        },
+        
+        //图书列表
+        list_main_images_by_books: function(isbns,cb) {
+            var url = host + "book/list_main_images_by_books?isbns=" + isbns;
             uu_request.get(url, function(err, response, body) {
                 if (!err && response.statusCode === 200) {
                     var info = JSON.parse(body);
